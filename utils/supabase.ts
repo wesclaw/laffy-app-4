1f91c8536188223003c96a2603384c871cb441a5
wesclaw <108541397+wesclaw@users.noreply.github.com>
2025-01-01 13:18:38 +0100

    up

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,19 +1,23 @@
-// import 'react-native-url-polyfill/auto';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-// import { createClient } from '@supabase/supabase-js';
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+
+//  "output": "static", to "output": "single" had to change this in the app.json for the code to work
+
 
 // import dotenv from 'dotenv';
 // dotenv.config();
-
 // const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string
 // const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string
 
+const supabaseUrl = "https://mduejjfymfqtmqbqdiwn.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kdWVqamZ5bWZxdG1xYnFkaXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUzMzE5MjksImV4cCI6MjA1MDkwNzkyOX0.hzTinYuY_vXwUJYsoLsyvi-AWtbPBwiVHDt46Vy-FzI"
 
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-//   auth: {
-//     storage: AsyncStorage,
-//     autoRefreshToken: true,
-//     persistSession: true,
-//     detectSessionInUrl: false,
-//   },
-// });
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
